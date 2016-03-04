f6064a8813262a2bd637a221e855fefae40cc3ff
mm.bivol@gmail.com <mm.bivol@gmail.com>
2016-03-04 10:38:06 +0100

    Load schema as a  so others can resolve nicely

--- validate.py
+++ validate.py
@@ -4,8 +4,8 @@ import json
 import jsonschema
 
 res1 = json.load(open('./res/res1.json'))
-res2 = json.load(open('./res/res1.json'))
-schema = json.load(open('./schemas/schema.json'))
+res2 = json.load(open('./res/res2.json'))
+schema = {'$ref': 'http://localhost:8000/schemas/schema.json'}
 
 print jsonschema.validate(res1, schema)
 print jsonschema.validate(res2, schema)
